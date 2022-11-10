334c8556deebca1ee24f164b3b1a75ce5ca54608
Gavin Mach <gavinmach@gmail.com>
2022-11-11 01:58:57 +1100

    Requirement 1 and 2 market orders limit orders and API mock server

--- src/App.js
+++ src/App.js
@@ -1,75 +1,49 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import OrderBook from "./OrderBook"
+import OrderEntry from "./OrderEntry"
+import './api/server'
 import "./App.scss"
-const tickSize = 0.25
-const askMax = 2971.50
-const askMin = 2967
-const bidMax = 2966.75
-const bidMin = 2963.50
-const maxSize = 300
-const minSize = 1
-
-const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step))
-const getRandom = (min, max) => Math.round(Math.random() * (max - min) + min)
-
-const asks = range(askMin, askMax, tickSize).map(e => [e, getRandom(minSize, maxSize)]).reverse()
-const bids = range(bidMin, bidMax, tickSize).map(e => [e, getRandom(minSize, maxSize)]).reverse()
-
 
 function App() {
   const [orderSize, setOrderSize] = useState(0)
+  const [orderPrice, setOrderPrice] = useState(0)
   const [book, setBook] = useState({
-    bids,
-    asks
+    bids: [],
+    asks: []
   })
 
-  const handleChange = (e) => {
+  useEffect(() => {
+    fetch("/api/orderbook")
+      .then((response) => response.json())
+      .then((json) => setBook(json))
+  }, [])
+
+  const handleSizeChange = (e) => {
     setOrderSize(e.target.value)
   }
 
-  const handleClickOrder = (price, orderAction) => {
-    const { bids, asks } = book;
-    let updatedQuotes = []
-    const isMarketSell = orderAction === 'marketSell'
-    const isMarketBuy = orderAction === 'marketBuy'
-    const isLimitBuy = orderAction === 'limitBuy'
-    const isLimitSell = orderAction === 'limitSell'
-    const size = parseInt(orderSize)
-    const quotes = isMarketBuy || isLimitSell ? asks : bids
-    if (isLimitBuy || isLimitSell)  { // limit order   
-      quotes.forEach(quote => {
-        if (quote[0] === price) 
-          quote[1] += size
-      })
-      updatedQuotes = [...quotes]
-    } else { // market order      
-      let filledSize = 0
-      const reducerFn = (previousValue, currentValue) => {
-        let [quotePrice, quoteSize] = currentValue
-        if (filledSize < size) {
-          const remainingSize = size - filledSize
-          if (remainingSize < quoteSize) {
-            filledSize += remainingSize
-            previousValue.push([ quotePrice, quoteSize - remainingSize ] )
-          } else 
-            filledSize += quoteSize
-        } else 
-          previousValue.push(currentValue)
-        return previousValue;
-      }
-      const processedQuotes = isMarketSell ? quotes.reduce(reducerFn, []) : quotes.reduceRight(reducerFn, []);        
-      updatedQuotes = [...processedQuotes]
-    }
-    const updatedBook = isMarketBuy || isLimitSell ? 
+  const handlePriceChange = (e) => {
+    setOrderPrice(e.target.value)
+  }
+
+  const handleClickOrder = async (orderPrice, orderAction) => {
+    setOrderPrice(orderPrice)
+    let response = await fetch("/api/orderbook", 
     {
-      asks : isLimitSell ? updatedQuotes : updatedQuotes.reverse(),
-      bids
-    } : 
-    {
-      asks, 
-      bids: updatedQuotes
-    } 
-    setBook(updatedBook)
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(
+        {
+          orderAction,
+          orderSize,
+          orderPrice
+        }
+      )
+    })
+    response = await response.json()
+    setBook(response)
   }
   
   return (
@@ -78,15 +52,7 @@ function App() {
         book={book}
         handleClickOrder={handleClickOrder}
       />
-      <label htmlFor="size">Enter # of contracts/size</label>
-      <input 
-        type="number"
-        value={orderSize} 
-        placeholder="10" 
-        step="1" 
-        min="1" 
-        onChange={handleChange}
-        name="size"/>
+     <OrderEntry orderSize={orderSize} handleSizeChange={handleSizeChange} orderPrice={orderPrice} handlePriceChange={handlePriceChange} />
     </>
   );
 };
---
+++ src/api/server.js
@@ -0,0 +1,81 @@
+import { createServer, Model } from "miragejs"
+
+const tickSize = 0.25
+const askMax = 2971.50
+const askMin = 2967
+const bidMax = 2966.75
+const bidMin = 2963.50
+const maxSize = 300
+const minSize = 1
+
+const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step))
+const getRandom = (min, max) => Math.round(Math.random() * (max - min) + min)
+
+const asks = range(askMin, askMax, tickSize).map(e => [e, getRandom(minSize, maxSize)]).reverse()
+const bids = range(bidMin, bidMax, tickSize).map(e => [e, getRandom(minSize, maxSize)]).reverse()
+let orderBook = 
+{
+  bids,
+  asks
+}
+
+const handlePost = (schema, request) => {
+  let attrs = JSON.parse(request.requestBody)
+  const orderAction = attrs.orderAction
+  const orderSize = parseInt(attrs.orderSize)
+  const orderPrice = attrs.orderPrice
+  const { bids, asks } = orderBook;
+  let updatedQuotes = []
+  const isMarketSell = orderAction === 'marketSell'
+  const isMarketBuy = orderAction === 'marketBuy'
+  const isLimitBuy = orderAction === 'limitBuy'
+  const isLimitSell = orderAction === 'limitSell'
+  const quotes = isMarketBuy || isLimitSell ? asks : bids
+  if (isLimitBuy || isLimitSell)  { // limit order   
+    quotes.forEach(quote => {
+      if (quote[0] === orderPrice) 
+        quote[1] += orderSize
+    })
+    updatedQuotes = [...quotes]
+  } else { // market order      
+    let filledSize = 0
+    const reducerFn = (previousValue, currentValue) => {
+      let [quotePrice, quoteSize] = currentValue
+      if (filledSize < orderSize) {
+        const remainingSize = orderSize - filledSize
+        if (remainingSize < quoteSize) {
+          filledSize += remainingSize
+          previousValue.push([ quotePrice, quoteSize - remainingSize ] )
+        } else 
+          filledSize += quoteSize
+      } else 
+        previousValue.push(currentValue)
+      return previousValue;
+    }
+    const processedQuotes = isMarketSell ? quotes.reduce(reducerFn, []) : quotes.reduceRight(reducerFn, []);        
+    updatedQuotes = [...processedQuotes]
+  }
+  const updatedBook = isMarketBuy || isLimitSell ? 
+  {
+    asks : isLimitSell ? updatedQuotes : updatedQuotes.reverse(),
+    bids
+  } : 
+  {
+    asks, 
+    bids: updatedQuotes
+  }
+  orderBook = { ...updatedBook }
+  // let schemaBook = schema.orderBooks.create(updatedBook)
+  return updatedBook
+}
+
+createServer({
+  models: {
+    orderBook: Model,
+  },
+  routes() {
+    this.get("/api/orderbook", () => orderBook)
+    this.post("/api/orderbook", handlePost)
+  },
+})
+
